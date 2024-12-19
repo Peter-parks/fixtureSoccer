b3a974838bd7a070cdac4b8d879b0877d73f3cf7
Pier_Cc <pier.cc.aj@gmail.com>
2024-12-19 11:31:16 -0300

    armando la estructura

--- fixture-app/src/index.js
+++ fixture-app/src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Cards from './componentes/cards/cards';
-import Footer from './componentes/footer/footer';
-import MenuBars from './componentes/menuBars/menuBars';
-import NavBar from './componentes/navBar/navBar';
-import Section from './componentes/section/section';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
-    <Section/>
     <App />
-    <Cards/>
-    <Footer/>
-    <MenuBars/>
   </React.StrictMode>
 );
 
